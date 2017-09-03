9ff4408342c51de0ceb52f747cbd83ff7c7b6a01
Walter <walter.cardozo@almundo.com>
2017-09-02 22:24:04 -0300

    Fixing warning in hand evaluator test

--- pokerestar-domain/src/test/java/com/waltercrdz/poker/test/domain/evaluator/HandEvaluatorTest.java
+++ pokerestar-domain/src/test/java/com/waltercrdz/poker/test/domain/evaluator/HandEvaluatorTest.java
@@ -1,15 +1,7 @@
 package com.waltercrdz.poker.test.domain.evaluator;
 
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
-
-import com.google.common.collect.Lists;
-import com.waltercrdz.poker.domain.Card;
-import com.waltercrdz.poker.domain.Rank;
-import com.waltercrdz.poker.domain.Suit;
-import com.waltercrdz.poker.domain.evaluator.HandEvaluator;
 
 public class HandEvaluatorTest {
 
@@ -20,24 +12,20 @@ public class HandEvaluatorTest {
 	@Test
 	public void test() {
 		// TODO to implements
-		final HandEvaluator evaluator;
-		List<Card> aHoleCards = this.createHoleCards();
-		List<Card> communityCards = this.createCommunityCards();
-		
 	}
 
-	private List<Card> createCommunityCards() {
-		final List<Card> holeCards = Lists.newArrayList();
-		holeCards.add(new Card.Builder().rank(Rank.ACE).suit(Suit.CLUB).build());
-		holeCards.add(new Card.Builder().rank(Rank.ACE).suit(Suit.DIAMOND).build());
-		return holeCards;
-	}
-
-	private List<Card> createHoleCards() {
-		final List<Card> holeCards = Lists.newArrayList();
-		holeCards.add(new Card.Builder().rank(Rank.ACE).suit(Suit.CLUB).build());
-		holeCards.add(new Card.Builder().rank(Rank.ACE).suit(Suit.DIAMOND).build());
-		return holeCards;
-	}
+//	private List<Card> createCommunityCards() {
+//		final List<Card> holeCards = Lists.newArrayList();
+//		holeCards.add(new Card.Builder().rank(Rank.ACE).suit(Suit.CLUB).build());
+//		holeCards.add(new Card.Builder().rank(Rank.ACE).suit(Suit.DIAMOND).build());
+//		return holeCards;
+//	}
+//
+//	private List<Card> createHoleCards() {
+//		final List<Card> holeCards = Lists.newArrayList();
+//		holeCards.add(new Card.Builder().rank(Rank.ACE).suit(Suit.CLUB).build());
+//		holeCards.add(new Card.Builder().rank(Rank.ACE).suit(Suit.DIAMOND).build());
+//		return holeCards;
+//	}
 
 }
